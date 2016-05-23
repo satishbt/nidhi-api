71078925cac9d82cd787e423d1a73e6ac5e6cc0a
sathishkumar bt <sathish.kumar.tippeshappa@ericsson.com>
2016-05-23 14:13:04 +0530

    first code

---
+++ src/test/java/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ApplicationTests {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+
+}
